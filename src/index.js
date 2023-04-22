051aae7485237ab358d6b04d5970ce1da2cd78fb
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-04-22 15:14:40 +0300

    add component

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ import './index.css';
 import { Profile } from 'components/profile/Profile';
 import user from './components/profile/user.json';
 
-// import { Statistics } from 'components/statistics/Statistics';
-// import data from './components/statistics/data.json';
+import { Statistics } from 'components/statistics/Statistics';
+import data from './components/statistics/data.json';
 
-// import { FriendList } from 'components/friend-list/FriendList';
-// import friends from './components/friend-list/friends.json';
+import { FriendList } from 'components/friend-list/FriendList';
+import friends from './components/friend-list/friends.json';
 
-// import { TransactionHistory } from 'components/transaction-history/TransactionHistory';
-// import transactions from './components/transaction-history/transactions.json';
+import { TransactionHistory } from 'components/transaction-history/TransactionHistory';
+import transactions from './components/transaction-history/transactions.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -25,10 +25,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} />
+      <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
-      <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </App>
   </React.StrictMode>
 );
