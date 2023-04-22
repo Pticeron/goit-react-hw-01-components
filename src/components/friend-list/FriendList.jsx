a692a8b309d880dbb64de3220dc96433f349c87e
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-04-22 15:29:18 +0300

    adjusts styles

--- src/components/friend-list/FriendList.jsx
+++ src/components/friend-list/FriendList.jsx
@@ -1,29 +1,28 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from './FriendListItem';
-import css from './Friend.module.css';
+import css from './Friend.module.css'
 
 export const FriendList = ({ friends }) => (
-    <ul className={css.friendList}>
-        {friends.map(friend => (
-            <FriendListItem
-            key={friend.id}
-            avatar={friend.avatar}
-            isOnline={friend.isOnline}
-            name={friend.name}
-        ></FriendListItem>
+<ul className={css.friendList}>
+    {friends.map(friend => (
+    <FriendListItem
+        key={friend.id}
+        avatar={friend.avatar}
+        isOnline={friend.isOnline}
+        name={friend.name}
+    ></FriendListItem>
     ))}
-    </ul>
+</ul>
 );
 
-
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            avatar: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            isOnline: PropTypes.bool.isRequired,
-        })
-    ).isRequired,
+friends: PropTypes.arrayOf(
+    PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    })
+).isRequired,
 };
 
