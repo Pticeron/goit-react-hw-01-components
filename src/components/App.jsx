22f8e98679f6e406ea4a6784c2039c8f8bea00b3
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-04-23 10:18:24 +0300

    corrects the mentor's remarks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Profile } from 'components/profile/Profile';
+import user from 'data/user.json';
+
+import { Statistics } from 'components/statistics/Statistics';
+import data from 'data/data.json';
+
+import { FriendList } from 'components/friend-list/FriendList';
+import friends from 'data/friends.json';
+
+import { TransactionHistory } from 'components/transaction-history/TransactionHistory';
+import transactions from 'data/transactions.json';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </App>
+  </React.StrictMode>
+);
+
 export const App = ({ children }) => {
   return (
     <div
--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
 import './index.css';
+import { App }  from './components/App';
 
-import { Profile } from 'components/profile/Profile';
-import user from './data/data.json';
-
-import { Statistics } from 'components/statistics/Statistics';
-import data from './data/data.json';
-
-import { FriendList } from 'components/friend-list/FriendList';
-import friends from './data/friends.json';
-
-import { TransactionHistory } from 'components/transaction-history/TransactionHistory';
-import transactions from './data/transactions.json';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </App>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
+
+
+// / import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// import { Profile } from 'components/profile/Profile';
+// import user from './data/user.json';
+
+// import { Statistics } from 'components/statistics/Statistics';
+// import data from './data/data.json';
+
+// import { FriendList } from 'components/friend-list/FriendList';
+// import friends from './data/friends.json';
+
+// import { TransactionHistory } from 'components/transaction-history/TransactionHistory';
+// import transactions from './data/transactions.json';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App>
+//       <Profile
+//         username={user.username}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+//       />
+//       <Statistics title="Upload stats" stats={data} />
+//       <Statistics stats={data} />
+//       <FriendList friends={friends} />
+//       <TransactionHistory items={transactions} />
+//     </App>
+//   </React.StrictMode>
+// );
